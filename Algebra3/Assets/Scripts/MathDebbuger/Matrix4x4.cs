712db70cbfbac4b4159da6f9542eb7f9fb5ba6cf
Matias <matiasmonti2011@hotmail.com>
2023-07-02 23:38:19 -0300

    Add All properties

--- Algebra3/Assets/Scripts/MathDebbuger/Matrix4x4.cs
+++ Algebra3/Assets/Scripts/MathDebbuger/Matrix4x4.cs
@@ -27,50 +27,227 @@ namespace CustomMath
         public float m13;
         public float m23;
         public float m33;
+        
+        //Inicializo una matriz en 0
+        private static readonly Matrix4x4 Zero = new Matrix4x4(new Vector4(0, 0, 0, 0), new Vector4(0, 0, 0, 0), new Vector4(0, 0, 0, 0), new Vector4(0, 0, 0, 0));
+
+        //Los elementos diagonales representan la escala y rotacion de identidad
+        private static readonly Matrix4x4 Identity = new Matrix4x4(new Vector4(1f, 0, 0, 0), new Vector4(0, 1f, 0, 0), new Vector4(0, 0, 1f, 0), new Vector4(0, 0, 0, 1f));
         #endregion
 
         #region Properties
-        private static readonly Matrix4x4 Zero = ;
+        public Vec3 lossyScale => GetLossyScale();
 
-        private static readonly Matrix4x4 Identity = ;
-
-        public Vec3 lossyScale
-        {
-
-        }
+        //Tiene como proposito calcular y devolver la escala "lossyScale"
+        //La escala lossyScale se refiere a la escala resultante de aplicar las transformaciones
         private Vec3 GetLossyScale()
         {
-            
+            //Representan los valores en diagonal de la matriz (X Y Z)
+            return new Vec3(m00, m11, m22);
         }
 
-        public Quat rotation;
+        public Quat rotation => GetRotation();
 
         private Quat GetRotation()
         {
-            
+            //Se calcula la suma de estos 3 componentes (X Y Z diagonales)
+            float trace = m00 + m11 + m22;
+            float w, x, y, z;
+
+            //Rotacion general
+            if (trace > 0)
+            {
+                //s = valor inverso multiplicativo de 2 * Raiz cuadrada del real + X Y Z
+                float s = 0.5f / Mathf.Sqrt(trace + 1);
+
+                //Calculo los componentes del cuaternion de rotacion
+                w = 0.25f / s;     //Esto se hace para normalizar, se divide por el factor escala
+                x = (m21 - m12) * s;
+                y = (m02 - m20) * s;
+                z = (m10 - m01) * s;
+            }
+            else if (m00 > m11 && m00 > m22) //Se hace para saber que componente de la matriz tiene el valor mas alto
+            {
+                float s = 2 * Mathf.Sqrt(1 + m00 - m11 - m22);
+                w = (m21 - m12) / s;
+                x = 0.25f * s;
+                y = (m01 + m10) / s;
+                z = (m02 + m20) / s;
+            }
+            else if (m11 > m22)
+            {
+                float s = 2 * Mathf.Sqrt(1 + m11 - m00 - m22);
+                w = (m02 - m20) / s;
+                x = (m01 + m10) / s;
+                y = 0.25f * s;
+                z = (m12 + m21) / s;
+            }
+            else
+            {
+                float s = 2 * Mathf.Sqrt(1 + m22 - m00 - m11);
+                w = (m10 - m01) / s;
+                x = (m02 + m20) / s;
+                y = (m12 + m21) / s;
+                z = 0.25f * s;
+            }
+
+            return new Quaternion(x, y, z, w);
         }
 
+        //Indexador nos permite acceder como si fuera un array
         public float this[int index]
         {
-            
+            get
+            {
+                return index switch
+                {
+                    0 => m00,
+                    1 => m10,
+                    2 => m20,
+                    3 => m30,
+                    4 => m01,
+                    5 => m11,
+                    6 => m21,
+                    7 => m31,
+                    8 => m02,
+                    9 => m12,
+                    10 => m22,
+                    11 => m32,
+                    12 => m03,
+                    13 => m13,
+                    14 => m23,
+                    15 => m33,
+                    _ => throw new IndexOutOfRangeException("Invalid matrix index!"),
+                };
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        m00 = value;
+                        break;
+                    case 1:
+                        m10 = value;
+                        break;
+                    case 2:
+                        m20 = value;
+                        break;
+                    case 3:
+                        m30 = value;
+                        break;
+                    case 4:
+                        m01 = value;
+                        break;
+                    case 5:
+                        m11 = value;
+                        break;
+                    case 6:
+                        m21 = value;
+                        break;
+                    case 7:
+                        m31 = value;
+                        break;
+                    case 8:
+                        m02 = value;
+                        break;
+                    case 9:
+                        m12 = value;
+                        break;
+                    case 10:
+                        m22 = value;
+                        break;
+                    case 11:
+                        m32 = value;
+                        break;
+                    case 12:
+                        m03 = value;
+                        break;
+                    case 13:
+                        m13 = value;
+                        break;
+                    case 14:
+                        m23 = value;
+                        break;
+                    case 15:
+                        m33 = value;
+                        break;
+                    default:
+                        throw new IndexOutOfRangeException("Invalid matrix index!");
+                }
+            }
         }
 
         public float this[int row, int column]
         {
-            
+            get
+            {
+                return this[row + column * 4];
+            }
+            set
+            {
+                this[row + column * 4] = value;
+            }
         }
 
-        public Matrix4x4 transpose;
+        public Matrix4x4 transpose => Transpose(this);
 
+        /*Crea una matriz donde se intercambian las filas y columnas, nos sirve para transformacion o calculos de orientacion.
+          Si bien cambia todo, las bases se siguen respetando es decir.
+          Original
+        
+         m00  m01  m02  m03
+         m10  m11  m12  m13
+         m20  m21  m22  m23
+         m30  m31  m32  m33
+         
+
+          Traspuesta
+        
+         m00  m10  m20  m30
+         m01  m11  m21  m31
+         m02  m12  m22  m32
+         m03  m13  m23  m33
+         */
         public static Matrix4x4 Transpose(Matrix4x4 m)
         {
-            
+            //Creo una instancia de una matriz para almacenar la traspuesta
+            Matrix4x4 result = new Matrix4x4();
+
+            //Bucle anidado para recorrer filas y columnas
+            for (int row = 0; row < 4; row++)
+            {
+                for (int column = 0; column < 4; column++)
+                {
+                    result[column, row] = m[row, column]; //En cada iteracion asigno i j de la matriz original a j i de la traspuesta
+                }
+            }
+
+            return result;
         }
 
-
+        //Se utiliza para crear una matriz a partir de 4 vectores de columna
         public Matrix4x4(Vector4 column0, Vector4 column1, Vector4 column2, Vector4 column3)
         {
-            
+            m00 = column0.x;
+            m01 = column1.x;
+            m02 = column2.x;
+            m03 = column3.x;
+
+            m10 = column0.y;
+            m11 = column1.y;
+            m12 = column2.y;
+            m13 = column3.y;
+
+            m20 = column0.z;
+            m21 = column1.z;
+            m22 = column2.z;
+            m23 = column3.z;
+
+            m30 = column0.w;
+            m31 = column1.w;
+            m32 = column2.w;
+            m33 = column3.w;
         }
         #endregion
 
